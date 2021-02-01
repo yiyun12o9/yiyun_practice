be09de0879054fc66271cd062f8315aaae5aaca8
yiyun12o9 <yoyolee7012@gmail.com>
2021-02-01 16:30:08 +0800

    Basic1

---
+++ Basic_Building_a_Graph/Assets/Scripts/Graph.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Graph : MonoBehaviour{
+    [SerializeField]
+    Transform pointPrefab=default;
+
+    [SerializeField,Range(10,100)]
+    int resolution = 10;
+
+    Transform[] points;
+    void Awake(){
+        float step = 2f / resolution;
+        var scale = Vector3.one * step ;
+        var position = Vector3.zero;
+        points = new Transform[resolution];
+        for (int i=0; i< points.Length; i++){
+            Transform point = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;
+            //position.y = position.x * position.x * position.x;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform,false);
+            points[i] = point;
+        }
+    }
+    void Update()
+    {
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
+        }
+    }
+}
---
+++ Basic_Clock/Assets/Scripts/Clock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    const float hoursToDegrees = -30f,minutesToDegrees=-6f,secondsToDegrees=-6f;
+
+    [SerializeField]
+    Transform hoursPivot=default, minutesPivot=default, secondsPivot=default;
+
+    void Update()
+    {
+        TimeSpan time=DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation=
+            Quaternion.Euler(0f,0f,hoursToDegrees*(float)time.TotalHours);
+        minutesPivot.localRotation = 
+            Quaternion.Euler(0f, 0f, minutesToDegrees * (float)time.TotalMinutes);
+        secondsPivot.localRotation = 
+            Quaternion.Euler(0f, 0f, secondsToDegrees * (float)time.TotalSeconds);
+    }
+}
