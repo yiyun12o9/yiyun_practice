545350acbfdfc8bffa697c65e4274810c8d20862
yiyun12o9 <yoyolee7012@gmail.com>
2021-05-30 22:52:56 +0800

    做完icon圖片

---
+++ UI_try/Assets/scripts/Lhand_try.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Lhand_try : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private SteamVR_Behaviour_Pose Pose;
+    public GameObject icon;
+    private void Awake()
+    {
+        Pose = GetComponent<SteamVR_Behaviour_Pose>();
+    }
+    private Vector3 Lhandmove;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        Lhandmove = Pose.GetVelocity();
+        //Lhandmove = m_Pose.GetPostion();
+
+        icon.transform.position = Lhandmove;
+    }
+
+
+    // Update is called once per frame
+
+}
