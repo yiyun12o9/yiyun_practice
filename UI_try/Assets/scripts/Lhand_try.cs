1ef2fa08e1dcafc0d5bf418d9eb8dc2011f2f1b4
yiyun12o9 <yoyolee7012@gmail.com>
2021-06-01 17:49:52 +0800

    調整好介面大小&位置

--- UI_try/Assets/scripts/Lhand_try.cs
+++ UI_try/Assets/scripts/Lhand_try.cs
@@ -8,21 +8,47 @@ public class Lhand_try : MonoBehaviour
     // Start is called before the first frame update
     private SteamVR_Behaviour_Pose Pose;
     public GameObject icon;
+    private Vector3 Lhandmove;
     private void Awake()
     {
         Pose = GetComponent<SteamVR_Behaviour_Pose>();
     }
-    private Vector3 Lhandmove;
+    public void paint()
+    {
+        Debug.Log("click button1");
+    }
+    public void palette()
+    {
+        Debug.Log("click button2");
+    }
+    public void eraser()
+    {
+        Debug.Log("click button3");
+    }
+    public void clear()
+    {
+        Debug.Log("click button4");
+    }
+    public void undo()
+    {
+        Debug.Log("click button5");
+    }
+    public void redo()
+    {
+        Debug.Log("click button6");
+    }
+    
     void Start()
     {
 
     }
     void Update()
     {
-        Lhandmove = Pose.GetVelocity();
-        //Lhandmove = m_Pose.GetPostion();
+        //Lhandmove = Pose.GetVelocity();
+        //Lhandmove = new Vector3(-100f, -100f, -100f);
+        //Lhandmove = Pose.GetPostion() * -1;
 
-        icon.transform.position = Lhandmove;
+        //icon.transform.position = Lhandmove;
     }
 
 
