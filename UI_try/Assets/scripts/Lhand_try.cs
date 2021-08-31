f11a47367b765aafd29dd329c4369052452870ee
yiyun12o9 <yoyolee7012@gmail.com>
2021-08-31 16:48:22 +0800

    canvas_pointer

--- UI_try/Assets/scripts/Lhand_try.cs
+++ UI_try/Assets/scripts/Lhand_try.cs
@@ -1,54 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Valve.VR;
 
 public class Lhand_try : MonoBehaviour
 {
     // Start is called before the first frame update
-    private SteamVR_Behaviour_Pose Pose;
+    public SteamVR_Action_Boolean GrabPinch;  //板機鍵按鈕
+    public SteamVR_Behaviour_Pose Pose;
     public GameObject icon;
-    private Vector3 Lhandmove;
+    public Vector3 Lhandmove;
+    public Button paint;
+    public bool pressed;
     private void Awake()
     {
         Pose = GetComponent<SteamVR_Behaviour_Pose>();
+        
     }
-    public void paint()
+
+    void Start()
+    {
+        icon.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(0f, 0.1f, 0.2f);
+        
+        //GetComponent<icon>().onClick.AddListener(paint);
+    }
+
+
+
+    public void Paint()
     {
         Debug.Log("click button1");
+        //icon.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(0.5f, 0.1f, 0.2f);
+        //icon.transform.position.x = 100;
     }
-    public void palette()
+    public void Palette()
     {
         Debug.Log("click button2");
     }
-    public void eraser()
+    public void Eraser()
     {
         Debug.Log("click button3");
     }
-    public void clear()
+    public void Clear()
     {
         Debug.Log("click button4");
     }
-    public void undo()
+    public void Undo()
     {
         Debug.Log("click button5");
     }
-    public void redo()
+    public void Redo()
     {
         Debug.Log("click button6");
     }
     
-    void Start()
-    {
 
-    }
     void Update()
     {
-        //Lhandmove = Pose.GetVelocity();
+        Lhandmove = Pose.GetVelocity();
         //Lhandmove = new Vector3(-100f, -100f, -100f);
         //Lhandmove = Pose.GetPostion() * -1;
 
-        //icon.transform.position = Lhandmove;
+        //icon.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(0.5f, 0.3f, 0f);
+        /*if (GrabPinch.GetStateDown(Pose.inputSource))
+        {
+            paint.onClick.AddListener(Paint);
+            paint.interactable = !paint.interactable;
+
+        }*/
+        //paint.onClick.AddListener(Paint);
+        /*if (paint.pressed == true)
+        {
+
+        }*/
+
     }
 
 
--- UI_try/Assets/scripts/Lhand_try2.cs
+++ UI_try/Assets/scripts/Lhand_try2.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
 
+[RequireComponent(typeof(Interactable))]
 public class Lhand_try2 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public SteamVR_Action_Boolean GrabPinch;  //板機鍵按鈕
+    public Button paint;
+
+    protected virtual void Awake()
+    {
+        paint = GetComponent<Button>();
+        if (paint)
+        {
+            paint.onClick.AddListener(Paint);
+        }
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    void Paint()
+    {
+        Debug.Log("click button1");
+    }
+
     void Update()
     {
         
