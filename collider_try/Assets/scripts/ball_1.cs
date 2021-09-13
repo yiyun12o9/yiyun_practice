c6460d84a9196a3ed3024295844dcdfd540d6bb6
yiyun12o9 <yoyolee7012@gmail.com>
2021-09-13 18:09:05 +0800

    collider

---
+++ collider_try/Assets/scripts/ball_1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball_1 : MonoBehaviour
+{
+    public GameObject Ball, Ball1;
+    Collider ballcollider;
+    void Start()
+    {
+        Ball = GameObject.Find("ball");
+        Ball1 = GameObject.Find("ball1");
+        ballcollider = GetComponent<Collider>();
+        //Ball.AddComponent<Collider>();
+        ballcollider.isTrigger = false;
+        
+    }
+
+    
+    void Update()
+    {
+        //if(OnCollisionEnter==true)
+    }
+    void OnCollisionEnter(Collision collision) 
+    {
+        Debug.Log("helloworld");
+    }
+}
---
+++ collider_try/Assets/scripts/button_2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button_2 : MonoBehaviour
+{
+    public GameObject Circle;
+    Collider circlecollider;
+    void Start()
+    {
+        Circle = GameObject.Find("circle");
+        circlecollider = GetComponent<Collider>();
+        //Ball.AddComponent<Collider>();
+        circlecollider.isTrigger = false;
+
+    }
+
+
+    void Update()
+    {
+        //if(OnCollisionEnter==true)
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Yo man");
+    }
+}
